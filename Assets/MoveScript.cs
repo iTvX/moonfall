afc8831e325b6991cee941baf15911a61492f904
Yang Chen <chen716@usc.edu>
2022-02-06 10:03:31 -0800

    add hook

---
+++ Assets/MoveScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    public float moveSpeed = 5f; 
+    public bool isGrounded = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	Jump();
+	Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),0f,0f);
+	transform.position += movement * Time.deltaTime * moveSpeed;
+        
+    }
+    void Jump(){
+	//print(isGrounded);
+	if(Input.GetButtonDown("Jump") && isGrounded == true){
+	gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,5f),ForceMode2D.Impulse);
+	}
+    }
+}
