1612d7f5f8e0f6d988a1701bc740c9468584c5b2
Yang Chen <chen716@usc.edu>
2022-02-10 22:31:16 -0800

    updating world's rotation to the fucking body

--- Assets/grapplingHook.cs
+++ Assets/grapplingHook.cs
@@ -8,10 +8,13 @@ public class grapplingHook : MonoBehaviour
 	DistanceJoint2D joint;
 	Vector3 targetPos;
 	RaycastHit2D hit;
-	public float distance = 10f;
+	public float distance = 2f;
 	public LayerMask mask;
 	public float step = 0.04f;
-
+	Vector2 tempPos;
+	Vector3 currentPos;
+	Vector2 anchorPos;
+	Vector2 lookDirection;
 	// Use this for initialization
 	void Start()
 	{
@@ -36,34 +39,49 @@ public class grapplingHook : MonoBehaviour
 		
 		if (Input.GetKeyDown(KeyCode.E))
 		{
+			currentPos = GetComponent<Rigidbody2D>().transform.position;
 			print("shooting hook");
 			targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			targetPos.z = 0;
-
-			hit = Physics2D.Raycast(transform.position, targetPos - transform.position, distance, mask);
+			print("target: "+targetPos);
+			print("current:" + currentPos);
+			print("dir: " + (targetPos - currentPos));
+			targetPos.z = 0;
+			//lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - currentPos;//transform.position;
+			//tempPos = transform.position;
+			//tempPos.x = tempPos.x * Mathf.Cos(transform.rotation.z);
+			//tempPos.y = tempPos.y * Mathf.Sin(transform.rotation.z);
+			//Debug.DrawLine(transform.position, lookDirection);
+			hit = Physics2D.Raycast(transform.position, targetPos- currentPos, distance, mask);
+			//targetPos - transform.position, distance, mask);
 
 			if (hit.collider != null )
 
 			{
-				print(transform.position);
-
-				print("hit");
+				/*print(transform.position);
+				print(currentPos);
+				print("hit");*/
+				print("hit point: "+hit.point);
+				
 				joint.enabled = true;
-				Vector2 connectPoint = hit.point - new Vector2(hit.collider.transform.position.x, hit.collider.transform.position.y);
+				
+				anchorPos = new Vector2(hit.collider.gameObject.GetComponent<Rigidbody2D>().transform.position.x, hit.collider.gameObject.GetComponent<Rigidbody2D>().transform.position.y);
+				print("anchor: " + anchorPos);
+				Vector2 connectPoint = hit.point - anchorPos;
 				connectPoint.x = connectPoint.x / hit.collider.transform.localScale.x;
 				connectPoint.y = connectPoint.y / hit.collider.transform.localScale.y;
 				Debug.Log(connectPoint);
-				joint.connectedAnchor = connectPoint;
+				
 
 				joint.connectedBody = hit.collider.gameObject.GetComponent<Rigidbody2D>();
-				joint.distance = Vector2.Distance(transform.position, hit.point);
-
+				joint.distance = Vector2.Distance(currentPos, hit.point);
+				joint.connectedAnchor = hit.collider.gameObject.GetComponent<Rigidbody2D>().transform.InverseTransformDirection(connectPoint);
 				line.enabled = true;
 				line.SetPosition(0, transform.position);
 				line.SetPosition(1, hit.point);
 
 				//line.GetComponent<roperatio>().grabPos = hit.point;
-				line.SetPosition(1, joint.connectedBody.transform.TransformPoint(joint.connectedAnchor));
+				//line.SetPosition(1, joint.connectedBody.transform.TransformPoint(joint.connectedAnchor));
 
 			}
 
