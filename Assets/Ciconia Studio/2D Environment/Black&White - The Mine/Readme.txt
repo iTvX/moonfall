ac038fcb6c36e619f287e39c82e86bbf6d536b06
Zijie Yu <zijieuu@gmail.com>
2022-02-10 18:03:31 -0800

    add asset

---
+++ Assets/Ciconia Studio/2D Environment/Black&White - The Mine/Readme.txt
@@ -0,0 +1,17 @@
+Readme - B&W The Mine
+_________________________
+
+
+The Demo scene included has been setup in Unity 2020.1.0 using the UniversalRP and need 2D SpriteShape to work correctly.
+The scene has been tested on all Unity version which support 2D SpriteShape.
+
+
+
+_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+
+
+Contact :
+
+If you have any questions, please feel free to contact us : contact@ciconia-studio.com
+
+More news on our work? Follow us on Twitter : twitter.com/CiconiaStudio
--- Assets/MoveScript.cs
+++ Assets/MoveScript.cs
@@ -17,6 +17,7 @@ public class MoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         Jump();
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * moveSpeed;
@@ -25,13 +26,13 @@ public class MoveScript : MonoBehaviour
     void Jump()
     {
         //print(isGrounded);
-        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        if (Input.GetKeyDown("w") && jumpCount > 0)
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 5f), ForceMode2D.Impulse);
             jumpCount--;
         }
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetKeyDown("w") && isGrounded)
         {
             jumpCount = 1;
         }
