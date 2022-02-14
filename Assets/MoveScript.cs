31b931eb285cec16bdd6d35b186dd7f16348d1ba
Zijie Yu <zijieuu@gmail.com>
2022-02-14 13:06:15 -0800

    add data

--- Assets/MoveScript.cs
+++ Assets/MoveScript.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoveScript : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public bool isGrounded = false;
-
+    private Vector3 lastPosition;
+    private float totalDistance;
     public int jumpCount = 1;
+    public Text distance;
+    public Text height;
+    public Text Timecount;
+    private Vector3 startpoint;
+    private int currentheight = 0;
+
+    private float timer;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        startpoint = transform.position;
+        lastPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -21,6 +33,23 @@ public class MoveScript : MonoBehaviour
         Jump();
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * moveSpeed;
+        //算总行程
+        Vector3 Julicha = transform.position - lastPosition;
+        float distancethisframe = Julicha.magnitude;
+        totalDistance += distancethisframe;
+        int intDistance = (int)totalDistance;
+        lastPosition = transform.position;
+        //算当前高度
+        Vector3 Gaoducha = transform.position - startpoint;
+        float heightcount = Gaoducha.magnitude;
+        currentheight = (int)heightcount;
+        //timer
+        timer += Time.deltaTime;
+        int intTimer = (int)timer;
+
+        distance.text = "Total Distance: " + intDistance.ToString() + "m";
+        height.text = "Height: " + currentheight.ToString() + "m";
+        Timecount.text = "Time: " + intTimer.ToString() + "s";
     }
 
     void Jump()
@@ -32,9 +61,10 @@ public class MoveScript : MonoBehaviour
             jumpCount--;
         }
 
-        if (Input.GetKeyDown("space") && isGrounded)
+        if (isGrounded)
         {
             jumpCount = 1;
         }
     }
+    
 }
