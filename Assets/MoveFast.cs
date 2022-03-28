44ae48cc08683314cc9a2c69602271831861c374
Kexin Chen <kchen264@wisc.edu>
2022-03-28 14:59:09 -0700

    add level 3

---
+++ Assets/MoveFast.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFast : MonoBehaviour
+{
+
+    public float min = 2f;
+    public float max = 3f;
+    public GameObject platform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        min = platform.transform.position.x;
+        max = platform.transform.position.x + 8f;
+    }
+
+   
+    // Update is called once per frame
+    void Update()
+
+    {
+        platform.transform.position = new Vector3(Mathf.PingPong(Time.time * 5, max - min) + min, transform.position.y, transform.position.z);
+    }
+}
