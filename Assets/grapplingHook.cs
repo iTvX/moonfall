906e419544e242b93c4f5f0b57034e7c07e7a6d2
Haopeng <hf0242a@student.american.edu>
2022-04-10 23:35:18 -0700

    hook

--- Assets/grapplingHook.cs
+++ Assets/grapplingHook.cs
@@ -4,115 +4,126 @@ using UnityEngine;
 
 public class grapplingHook : MonoBehaviour
 {
-	public LineRenderer line;
-	DistanceJoint2D joint;
-	Vector3 targetPos;
-	RaycastHit2D hit;
-	RaycastHit2D hitTemp;
-	public float distance = 2f;
-	public LayerMask mask;
-	public float step = 0.04f;
-	Vector2 tempPos;
-	Vector3 currentPos;
-	Vector2 anchorPos;
-	Vector2 lookDirection;
-	public Camera cam;
-	// Use this for initialization
-	void Start()
-	{
-		joint = GetComponent<DistanceJoint2D>();
-		joint.enabled = false;
-		line.enabled = false;
-	}
+    public Camera cam;
+    public LineRenderer line;
+    DistanceJoint2D joint;
+    Vector3 targetPos;
+    RaycastHit2D hit;
+    public float distance = 2f;
+    public LayerMask mask;
+    public float step = 0.04f;
+    GameObject hitCollide;
+    Vector2 tempPos;
+    Vector3 currentPos;
+    Vector2 anchorPos;
+    Vector2 lookDirection;
+    Vector2 connectPoint;
 
-	// Update is called once per frame
-	void Update()
-	{
-		
-		if (joint.distance > .5f && joint.connectedBody != null)
-		{
-			if (joint.connectedBody.tag != "wall")
-			{
-				joint.distance -= step;
-			}
-		}
-		/*else
-		{
-			print("disable hook rn");
-			line.enabled = false;
-			joint.enabled = false;
+    // Use this for initialization
+    void Start()
+    {
+        joint = GetComponent<DistanceJoint2D>();
+        joint.enabled = false;
+        line.enabled = false;
+    }
 
-		}*/
+    // Update is called once per frame
+    void Update()
+    {
 
-		
-		if (Input.GetKeyDown(KeyCode.E))
-		{
-			currentPos = GetComponent<Rigidbody2D>().transform.position;
-			print("shooting hook");
-			targetPos = cam.ScreenToWorldPoint(Input.mousePosition);
-			targetPos.z = 0;
-			print("target: "+targetPos);
-			print("current:" + currentPos);
-			print("dir: " + (targetPos - currentPos));
-			targetPos.z = 0;
-			//lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - currentPos;//transform.position;
-			//tempPos = transform.position;
-			//tempPos.x = tempPos.x * Mathf.Cos(transform.rotation.z);
-			//tempPos.y = tempPos.y * Mathf.Sin(transform.rotation.z);
-			//Debug.DrawLine(transform.position, lookDirection);
-			hit = Physics2D.Raycast(transform.position, targetPos- currentPos, distance, mask);
-			//targetPos - transform.position, distance, mask);
+        if (joint.distance > .5f && joint.connectedBody != null)
+        {
+            if (joint.connectedBody.tag != "wall")
+            {
+                joint.distance -= step;
+            }
+        }
+        /*else
+        {
+         print("disable hook rn");
+         line.enabled = false;
+         joint.enabled = false;
 
-			if (hit.collider != null )
+        }*/
 
-			{
-				print(transform.position);
-				print(currentPos);
-				print("hit");
-				print("hit point: "+hit.point);
-				
-				joint.enabled = true;
-				
-				anchorPos = new Vector2(hit.collider.gameObject.GetComponent<Rigidbody2D>().transform.position.x, hit.collider.gameObject.GetComponent<Rigidbody2D>().transform.position.y);
-				print("anchor: " + anchorPos);
-				Vector2 connectPoint = hit.point - anchorPos;
-				connectPoint.x = connectPoint.x / hit.collider.transform.localScale.x;
-				connectPoint.y = connectPoint.y / hit.collider.transform.localScale.y;
-				Debug.Log(connectPoint);
-				Debug.Log("hit again:");
-				//Vector2 v2 = ( 0, 0 );
-				joint.anchor = new Vector2(0,0);
-				Vector3 tempp = hit.collider.gameObject.GetComponent<Rigidbody2D>().transform.InverseTransformDirection(connectPoint);
-				tempp.z = 0;
-				Debug.Log(tempp);
-				joint.connectedBody = hit.collider.gameObject.GetComponent<Rigidbody2D>();
-				joint.distance = Vector2.Distance(currentPos, hit.point);
-				joint.connectedAnchor = tempp;
-				line.enabled = true;
-				line.SetPosition(0, transform.position);
-				line.SetPosition(1, hit.point);
 
-				//line.GetComponent<roperatio>().grabPos = hit.point;
-				//line.SetPosition(1, joint.connectedBody.transform.TransformPoint(joint.connectedAnchor));
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            currentPos = GetComponent<Rigidbody2D>().transform.position;
+            // print("shooting hook");
+            targetPos = cam.ScreenToWorldPoint(Input.mousePosition);
+            targetPos.z = 0;
+            // print("target: "+targetPos);
+            // print("current:" + currentPos);
+            // print("dir: " + (targetPos - currentPos));
+            targetPos.z = 0;
+            //lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - currentPos;//transform.position;
+            //tempPos = transform.position;
+            //tempPos.x = tempPos.x * Mathf.Cos(transform.rotation.z);
+            //tempPos.y = tempPos.y * Mathf.Sin(transform.rotation.z);
+            //Debug.DrawLine(transform.position, lookDirection);
+            hit = Physics2D.Raycast(transform.position, targetPos - currentPos, distance, mask);
+            //targetPos - transform.position, distance, mask);
 
-			}
+            if (hit.collider != null)
 
-		}
+            {
+                print(transform.position);
+                print(hit.point);
+                print("hit");
+                // print("hit point: "+hit.point);
 
-		if (Input.GetKey(KeyCode.E))
-		{
+                joint.enabled = true;
+                transform.parent = hit.collider.transform;
+                anchorPos = new Vector2(hit.collider.gameObject.GetComponent<Rigidbody2D>().transform.position.x, hit.collider.gameObject.GetComponent<Rigidbody2D>().transform.position.y);
+                print("anchor: " + anchorPos);
+                hitCollide = hit.collider.gameObject;
+                Vector2 tempConnect;
+                connectPoint = hit.point - anchorPos;
+                tempConnect = connectPoint;
+                tempConnect.x = tempConnect.x / hit.collider.transform.localScale.x;
+                tempConnect.y = tempConnect.y / hit.collider.transform.localScale.y;
+                Debug.Log(connectPoint);
 
-			line.SetPosition(0, transform.position);
-		}
-		
 
-		if (Input.GetKeyUp(KeyCode.E))
-		{
-			
-			joint.enabled = false;
-			line.enabled = false;
-		}
-		
-	}
+                joint.connectedBody = hit.collider.gameObject.GetComponent<Rigidbody2D>();
+                joint.anchor = new Vector2(0, 0);
+                joint.distance = Vector2.Distance(currentPos, hit.point);
+                joint.connectedAnchor = hit.collider.gameObject.GetComponent<Rigidbody2D>().transform.InverseTransformDirection(tempConnect);
+                line.enabled = true;
+                line.SetPosition(0, transform.position);
+                line.SetPosition(1, hit.point);
+
+                //line.GetComponent<roperatio>().grabPos = hit.point;
+                //line.SetPosition(1, joint.connectedBody.transform.TransformPoint(joint.connectedAnchor));
+
+            }
+
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+
+            line.SetPosition(0, transform.position);
+            if (joint.enabled)
+            {
+                Vector3 temp = hitCollide.transform.position;
+                temp.x += connectPoint.x;
+                temp.y += connectPoint.y;
+                line.SetPosition(1, temp);
+            }
+        }
+
+
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            transform.parent = null;
+            joint.enabled = false;
+            line.enabled = false;
+        }
+
+    }
+
+
 
 }
