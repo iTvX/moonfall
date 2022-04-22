9afb28b72b698a66505b77866f301322f352d255
AbyssV <yatinl4@uci.edu>
2022-04-22 11:06:59 -0700

    add boundry and tutorial

--- Assets/CartMovement.cs
+++ Assets/CartMovement.cs
@@ -7,6 +7,10 @@ public class CartMovement : MonoBehaviour
     private Rigidbody2D rb;
     public GameObject endPoint;
     public float speed = 0.1f;
+    //public bool flag = true;
+    public Vector2 endPoint_Pos;
+    public float player_Pos;
+   
 
     // Start is called before the first frame update
     void Start()
@@ -18,22 +22,39 @@ public class CartMovement : MonoBehaviour
     void Update()
     {
         rb = GetComponent<Rigidbody2D>();
+        endPoint_Pos = endPoint.transform.position;
+      
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
+        
         if (collision.gameObject.tag == "Player")
         {
-            if (rb.transform.position.x != endPoint.transform.position.x)
-            {
-                rb.transform.Translate(Vector3.left * speed * Time.deltaTime);
-                collision.transform.Translate(Vector3.left * speed * Time.deltaTime);
-                //rb.velocity = new Vector2(-speed, 0);
-                //collision.rigidbody.velocity = new Vector2(-speed, 0);
+            //transform.position = Vector2.MoveTowards(transform.position, endPoint_Pos, speed * Time.deltaTime);         
+            //player_Pos = collision.transform.position.y;
+            //collision.transform.position = Vector2.MoveTowards(collision.transform.position, new Vector2(endPoint_Pos.x, player_Pos), speed * Time.deltaTime);
 
+            if (System.Math.Abs(transform.position.x - endPoint_Pos.x) > 1)
+            {
+                transform.Translate(Vector3.left * speed * Time.deltaTime);
+                collision.transform.Translate(Vector3.left * speed * Time.deltaTime);
             }
+
+            //if (transform.position.x != endPoint_Pos.x)
+            //{
+            //    transform.Translate(Vector3.left * speed * Time.deltaTime);
+            //    collision.transform.Translate(Vector3.left * speed * Time.deltaTime);
+            //}
+
 
         }
         
     }
+
+    //private void OnCollisionExit2D(Collision2D collision)
+    //{
+    //    flag = false;
+    //}
+
 }
